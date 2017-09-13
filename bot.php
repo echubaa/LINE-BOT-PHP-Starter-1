202e5e880b6cae957fb21603b6773d6fe26f7e3f
chutinan <echubaa36@gmail.com>
2017-09-13 17:25:36 +0700

    bot

--- bot.php
+++ bot.php
@@ -18,21 +18,21 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
+				{
+                    'type' => 'text',
+				    'text' => $text
+                },
+                {
+                    'type' => 'text',
+    				'text' => $text
+                }
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
-                {
-                    "type":"text",
-                    "text":"Hello, world1"
-                },
-                {
-                    "type":"text",
-                    "text":"Hello, world2"
-                }
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
