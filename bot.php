ededad2c6d4060898dd12790e903139150576f9f
chutinan <echubaa36@gmail.com>
2017-09-14 15:52:35 +0700

    ลืม ;

--- bot.php
+++ bot.php
@@ -38,7 +38,7 @@ if (!is_null($events['events'])) {
 				'type' => 'sticker',
  				'packageId' => '4',
  				'stickerId' => '300'
-			]
+			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
