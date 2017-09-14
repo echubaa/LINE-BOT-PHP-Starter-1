d786d7c3439f706c637a7855c81073dccb67db1f
chutinan <echubaa36@gmail.com>
2017-09-14 15:23:10 +0700

    ข้อความต่างกัน

--- bot.php
+++ bot.php
@@ -20,12 +20,15 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
-
+			$messages2 = [
+				'type' => 'text',
+				'text' => "มีอะไรให้ช่วยไหม"
+			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages,"มีอะไรให้ช่วยไหม?"],
+				'messages' => [$messages,$messages2],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
