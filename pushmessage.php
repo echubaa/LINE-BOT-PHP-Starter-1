0b46181809016f9451a6a82fa293c15d9443faee
chutinan <echubaa36@gmail.com>
2017-09-18 15:47:21 +0700

    multi message

--- pushmessage.php
+++ pushmessage.php
@@ -1,7 +1,9 @@
 <?php
 $access_token = 'sIJ4G+8/3gh0q2ISi9WI1CGB8QwU57IzK+2N+Ni16/zzF6rHBTwOryDhV8aaXvksMrqXa3//kU5t4PaJfMOD9HVisCHqPOa/1F9BSe0H8zlRYNXbK0YAMi29/+gbHfh2p9ed3PzskUKGzqIloikGGwdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v2/bot/message/push';
+// $url = 'https://api.line.me/v2/bot/message/push';
+$url = 'https://api.line.me/v2/bot/message/multicast';
+
 $replyId = ["Uec872a16e2dea9e3551b8a6ff583f684","Uef99b849d5948f2804d76f19ef9cc750"];
 $messages = [
     'type' => 'text',
