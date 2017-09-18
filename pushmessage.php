556749a66f68356489183e74b5618d07651f5897
chutinan <echubaa36@gmail.com>
2017-09-18 15:13:32 +0700

    push message

---
+++ pushmessage.php
@@ -0,0 +1,22 @@
+<?php
+$access_token = 'sIJ4G+8/3gh0q2ISi9WI1CGB8QwU57IzK+2N+Ni16/zzF6rHBTwOryDhV8aaXvksMrqXa3//kU5t4PaJfMOD9HVisCHqPOa/1F9BSe0H8zlRYNXbK0YAMi29/+gbHfh2p9ed3PzskUKGzqIloikGGwdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v2/bot/message/push';
+$replyId = 'Uec872a16e2dea9e3551b8a6ff583f684';
+$data = [
+    'to' => $replyId,
+    'messages' => [$messages,$messages2,$messages3],
+];
+$post = json_encode($data);
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
